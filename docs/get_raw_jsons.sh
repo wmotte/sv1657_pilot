2afd792d3d7f8df89052d0c9282576a4407a3818
Wim Otte <wmotte@gmail.com>
2025-11-11 15:15:29 +0100

    new structure

---
+++ docs/get_raw_jsons.sh
@@ -0,0 +1,5 @@
+cp ../../sv1657_modernisatie/output.editor/1SA/1SA.1.json .
+#cp ../../sv1657_modernisatie/output.editor/1SA/1SA.2.json .
+
+cp ../../sv1657_modernisatie/output.neerlandicus/1SA/1SA.1.json interim_1SA.1.json
+cp ../../sv1657_modernisatie/output.neerlandicus/1SA/1SA.2.json interim_1SA.2.json
