7b70534b855d5068cc7ea9de58b2a0edd38a9ef2
Wim Otte <wmotte@gmail.com>
2025-12-01 11:04:57 +0100

    new font

--- docs/get_raw_jsons.sh
+++ docs/get_raw_jsons.sh
@@ -1,6 +1,3 @@
-cp ../../sv1657_modernisatie/output.editor/1SA/1SA.1.json .
-cp ../../sv1657_modernisatie/output.editor/1SA/1SA.2.json .
-cp ../../sv1657_modernisatie/output.editor/1SA/1SA.3.json .
 
 cp ../../sv1657_modernisatie/output.neerlandicus/1SA/1SA.1.json interim_1SA.1.json
 cp ../../sv1657_modernisatie/output.neerlandicus/1SA/1SA.2.json interim_1SA.2.json
--- docs/inputs/get_data.sh
+++ docs/inputs/get_data.sh
@@ -1,1 +1,2 @@
 cp -r ../../../sv1657_modernisatie/output.editor/1SA .
+cp -r ../../../sv1657_modernisatie/output.editor/1JN .
