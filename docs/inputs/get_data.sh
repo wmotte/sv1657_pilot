ac43c533c92f5795903cb5a1aeb612f1a3c19df1
Wim Otte <wmotte@gmail.com>
2025-12-02 14:22:33 +0100

    MIC

--- docs/inputs/get_data.sh
+++ docs/inputs/get_data.sh
@@ -1,3 +1,4 @@
 cp -r ../../../sv1657_modernisatie/output.editor/1SA .
+cp -r ../../../sv1657_modernisatie/output.editor/MIC .
 cp -r ../../../sv1657_modernisatie/output.editor/1JN .
 cp -r ../../../sv1657_modernisatie/output.editor/MRK .
