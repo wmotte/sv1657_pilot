062f7afdb57c2590ed16910942cb2c2b22604a49
Wim Otte <wmotte@gmail.com>
2025-12-04 09:08:52 +0100

    HEB 1

--- docs/inputs/get_data.sh
+++ docs/inputs/get_data.sh
@@ -2,3 +2,4 @@ cp -r ../../../sv1657_modernisatie/output.final/1SA .
 cp -r ../../../sv1657_modernisatie/output.final/MIC .
 cp -r ../../../sv1657_modernisatie/output.final/1JN .
 cp -r ../../../sv1657_modernisatie/output.final/MRK .
+cp -r ../../../sv1657_modernisatie/output.final/HEB .
